da561f6bb4a2f2f44e248b952b1755450975f26f
Samuel Fayisa <4samuelfayisa@gmail.com>
2026-05-04 13:45:16 +0300

    focus_jit_bot.py

--- focus_jit_bot.py
+++ focus_jit_bot.py
@@ -60,7 +60,7 @@ GROUP_CHAT_ID = os.environ.get("GROUP_CHAT_ID", "YOUR_GROUP_CHAT_ID_HERE")
 # Numeric Telegram user IDs of leaders/admins.
 # Get yours by messaging @userinfobot on Telegram.
 ADMIN_IDS = {
-    8097004980,
+    838712052,
 }
 _raw_ids = os.environ.get("ADMIN_IDS", "")
 ADMIN_IDS = set(int(i.strip()) for i in _raw_ids.split(",") if i.strip())
